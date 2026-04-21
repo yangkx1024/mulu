ae03e31c53f44e96f3f6fd7e91e6799b46c2881c
Kexuan Yang <kexuan.yang@gmail.com>
2026-04-21 16:01:37 +0800

    Fix mac app package issue
    Signed-off-by: Kexuan Yang <kexuan.yang@gmail.com>

--- package-mac.sh
+++ package-mac.sh
@@ -140,22 +140,25 @@ fi
 info "embedding provisioning profile..."
 cp "$MAS_PROVISIONING_PROFILE" "$APP_PATH/Contents/embedded.provisionprofile"
 
+info "stripping extended attributes (incl. com.apple.quarantine)..."
+xattr -cr "$APP_PATH"
+
 info "writing entitlements..."
 cat > "$ENTITLEMENTS" <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
+    <key>com.apple.application-identifier</key>
+    <string>${APPLE_TEAM_ID}.net.yangkx.mulu</string>
+    <key>com.apple.developer.team-identifier</key>
+    <string>${APPLE_TEAM_ID}</string>
     <key>com.apple.security.app-sandbox</key>
     <true/>
     <key>com.apple.security.device.usb</key>
     <true/>
     <key>com.apple.security.files.user-selected.read-write</key>
     <true/>
-    <key>com.apple.security.application-groups</key>
-    <array>
-        <string>${APPLE_TEAM_ID}.net.yangkx.mulu</string>
-    </array>
 </dict>
 </plist>
 EOF
