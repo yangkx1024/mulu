8c3a2f4f9c68522c63686f324e24c89e9286aed7
Kexuan Yang <kexuan.yang@gmail.com>
2026-04-23 11:02:24 +0800

    Show version label to status bar
    Signed-off-by: Kexuan Yang <kexuan.yang@gmail.com>

--- src/mtp_browser/status_bar.rs
+++ src/mtp_browser/status_bar.rs
@@ -5,6 +5,8 @@ use rust_i18n::t;
 
 use super::MtpBrowser;
 
+const VERSION_LABEL: &str = concat!("v", env!("CARGO_PKG_VERSION"));
+
 impl MtpBrowser {
     pub(super) fn render_status_bar(&self, cx: &mut Context<Self>) -> AnyElement {
         let status_text = self
@@ -20,22 +22,28 @@ impl MtpBrowser {
             .text_xs()
             .text_color(cx.theme().muted_foreground)
             .child(status_text)
-            .when_some(self.update_info.as_ref(), |row, info| {
-                let notice = t!("status.update_available", version = info.version.as_str());
-                let url: SharedString = info.url.clone().into();
-                row.child(
-                    div()
-                        .id("update-notice")
-                        .ml_auto()
-                        .cursor_pointer()
-                        .text_color(cx.theme().primary)
-                        .hover(|s| s.underline())
-                        .child(SharedString::from(notice))
-                        .on_click(cx.listener(move |_, _, _, cx| {
-                            cx.open_url(url.as_ref());
-                        })),
-                )
-            })
+            .child(
+                h_flex()
+                    .ml_auto()
+                    .gap_3()
+                    .child(VERSION_LABEL)
+                    .when_some(self.update_info.as_ref(), |row, info| {
+                        let notice =
+                            t!("status.update_available", version = info.version.as_str());
+                        let url: SharedString = info.url.clone().into();
+                        row.child(
+                            div()
+                                .id("update-notice")
+                                .cursor_pointer()
+                                .text_color(cx.theme().primary)
+                                .hover(|s| s.underline())
+                                .child(SharedString::from(notice))
+                                .on_click(cx.listener(move |_, _, _, cx| {
+                                    cx.open_url(url.as_ref());
+                                })),
+                        )
+                    }),
+            )
             .into_any_element()
     }
 }
